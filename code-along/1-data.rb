8cc1259c2f3724d3edf5014a5cf7703bca85e1f3
mia-nam <mia.nam@kellogg.northwestern.edu>
2022-01-25 15:40:00 +0000

    break1

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -13,7 +13,26 @@
 # Combine strings together
 
 # Variables
+x = 10
+y = 3
+puts x * y
+
+food = "tacos"
+quantity = 3
+puts food * quantity
 
 # Combine strings and variables
+first_name = "Boba"
+greeting = "hello,  #{first_name}" #String interpolation
+puts greeting
+puts "#{food}: #{quantity}"
 
 # String manipulation
+puts "Hello".reverse
+puts "hello how are you".capitalize
+puts "hello".length
+
+creed ="This Is The Way"
+puts creed.upcase
+puts creed.downcase
+puts creed.swapcase
--- labs/1-data.rb
+++ labs/1-data.rb
@@ -9,6 +9,13 @@
 # dice. Set two variables, with names that make sense,
 # then write them out to the screen. Show the total
 # as well.
+number1 = rand(1..6)
+puts "first number is #{number1}"
+number2 = rand(1..6)
+total = number1+ number2
+puts "second number is #{number2}"
+
+puts "total is #{total}"
 
 # HINTS
 # Get a random number between 1 and 6 by doing:
@@ -20,3 +27,15 @@
 # "i would like " + 5 + " tacos"
 # To rectify this, you can convert the number into a string
 # "i would like #{5} tacos"
+
+# set the first die and write it to screen
+die1 = rand(1..6)
+puts die1
+
+# set the second die and write it to screen
+die2 = rand(1..6)
+puts die2
+
+#create a variable for the total and write it to screen
+totald = die1 + die2
+puts "The total is: #{totald}"
